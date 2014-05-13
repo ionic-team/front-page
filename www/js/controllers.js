bafae1f7cbccdfe107268cead658bcb8fe16c331
Perry Govier <govierp@gmail.com>
2014-05-13 17:32:53 -0500

    infinite scroll

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,21 +2,41 @@ angular.module('frontpage.controllers', [])
 
 .controller('FrontPageCtrl', function($scope, HNAPI, RequestCache) {
   $scope.posts = RequestCache.get('frontpage/0');
+  var currentPage = 0;
   HNAPI.frontpage(0).then(function(posts){
     $scope.posts = posts;
   });
   $scope.open = function(url){
     window.open(url,'_system');
   }
+  $scope.loadMoreData = function(){
+    currentPage++;
+    console.log('loading page '+currentPage);
+    HNAPI.frontpage(currentPage).then(function(posts){
+      $scope.posts = $scope.posts.concat(posts);
+      $scope.$broadcast('scroll.infiniteScrollComplete');
+      console.log($scope.posts);
+    });
+  }
 })
 
 .controller('NewestCtrl', function($scope, HNAPI, RequestCache) {
   $scope.posts = RequestCache.get('frontpage/0');
+  var currentPage = 0;
   HNAPI.newest(0).then(function(posts){
     $scope.posts = posts;
   });
   $scope.open = function(url){
     window.open(url,'_system');
+  }
+  $scope.loadMoreData = function(){
+    currentPage++;
+    console.log('loading page '+currentPage);
+    HNAPI.newest(currentPage).then(function(posts){
+      $scope.posts = $scope.posts.concat(posts);
+      $scope.$broadcast('scroll.infiniteScrollComplete');
+      console.log($scope.posts);
+    });
   }
 })
 
