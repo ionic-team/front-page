678680811ec76ac4117688b7a0d4277c3be603e3
Perry Govier <govierp@gmail.com>
2014-05-13 11:00:08 -0500

    cleaning up, changing name to front page

--- www/js/services.js
+++ www/js/services.js
@@ -1,37 +1,44 @@
 angular.module('starter.services', [])
 
 /**
- * A service that calls out to Algolia's Hacker News Search API
+ * A service that calls out to Drifty's Hacker News API
  */
 
-.factory('Algolia', function($rootScope, $http, $q) {
-  var apiURL = 'https://hn.algolia.com/api/v1/';
+.factory('HNAPI', function($rootScope, $http, $q) {
+  var apiURL = 'http://127.0.0.1:8080/';
   // load saved data if available
-  var posts = typeof localStorage.posts === 'undefined'? []:localStorage.posts;
+  var posts = typeof localStorage.posts === 'undefined'? {}:localStorage.posts;
 
   function update(){
-    $http.get(apiURL+'search_by_date?tags=(story,poll)')
-         .then(function(result){
-           posts = result.data.hits;
-           $rootScope.$broadcast('newPostsAvailable',posts);
-           localStorage.posts = posts;
-         });
+    function validateResponse(result){
+      console.log(result);
+      if(typeof result.data != 'array' || typeof result.data != 'object' )return false;
+
+      return true;
+    }
   }
 
   return {
     // get all recent posts
-    all: function() {
-      // get updated data from the server, but deliver what we have now too
-      update();
-      return posts;
+    frontpage: function(page) {
+      var q = $q.defer();
+      $http.get(apiURL+'frontpage/'+page)
+        .then(function(result){
+          if(!validateResponse(result))return q.reject(new Error('Invalid Response'));
+          q.resolve(result.data);
+        },function(err){
+          console.log('Search Failed');
+          q.reject(err);
+        });
+      return q.promise;
     },
     search: function(searchTerm) {
+      // call
       var q = $q.defer();
-      $http.get(apiURL+'search?tags=story&query='+searchTerm)
+      $http.get(apiURL+'search?&q='+searchTerm)
            .then(function(result){
-             console.log(result.data.hits);
-             posts = result.data.hits;
-             q.resolve(result.data.hits);
+             if(!validateResponse(result))return q.reject(new Error('Invalid Response'));
+             q.resolve(result.data);
            },function(err){
              console.log('Search Failed');
              q.reject(err);
@@ -39,37 +46,6 @@ angular.module('starter.services', [])
       return q.promise;
     }
   }
-})
-
-/**
- * A service that scrapes the Hacker News website for post data
- */
-
-.factory('HackerNewsScraper', function($rootScope, $http, $q) {
-  var hnURL = 'https://news.ycombinator.com/';
-
-  function parseNewHTML(html){
-    var el = document.createElement( 'div' );
-    el.innerHTML = html;
-    console.log(el);
-  }
-
-  return {
-    new:function(){
-      var q = $q.defer();
-      $http.get(hnURL)
-        .then(function(result){
-          console.log(result);
-          posts = parseNewHTML(result.data);
-          q.resolve(result);
-        },function(err){
-          console.log('Request Failed');
-          q.reject(err);
-        });
-      return q.promise;
-    }
-  }
-
 })
 ;
 
